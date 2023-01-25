364b8ccd61f36333bb8c5231b9678b574785007a
Isaiah peter <peterisaiah4fun@gmail.com>
2023-01-26 00:44:52 +0100

    task 1 and 2

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+export default function getResponseFromAPI() {
+    const newPromise = new Promise((resolve, reject) => {
+        return resolve
+    });
+
+    return newPromise
+}
---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,16 @@
+export default function getResponseFromAPI(success) {
+    const newPromise = new Promise((resolve, reject) => {
+        if (success) {
+            resolve(
+                {
+                    status: 200,
+                    body: 'Success'
+                }
+            )
+        } else {
+            reject("Error: The fake API is not working currently")
+        }
+    });
+
+    return newPromise
+}
