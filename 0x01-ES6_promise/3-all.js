ddc38034305168d935886518129b5cb8ed7b1319
Isaiah peter <peterisaiah4fun@gmail.com>
2023-01-26 01:34:47 +0100

    task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,19 @@
+import { uploadPhoto, createUser } from "./utils";
+
+export default function handleProfileSignup() {
+  let string = "";
+
+  uploadPhoto().then(
+    (value) => {
+      string += value.body;
+    },
+    () => console.log("Signup system offline")
+  );
+  createUser().then(
+    (value) => {
+      string += ` ${value.firstName} ${value.lastName}`;
+      console.log(string);
+    },
+    () => console.log("Signup system offline")
+  );
+}
