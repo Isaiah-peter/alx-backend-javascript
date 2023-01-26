423daeaf325491a2ca747ef86161f71ec9db1b3d
Isaiah peter <peterisaiah4fun@gmail.com>
2023-01-26 02:27:12 +0100

    task 100

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -3,14 +3,16 @@ import { uploadPhoto, createUser } from './utils';
 export default async function asyncUploadUser() {
   const promise1 = await uploadPhoto().then(
     (value) => value,
+    () => null,
   );
 
   const promise2 = await createUser().then(
     (value) => value,
+    () => null,
   );
 
   return {
-    photo: promise1 || null,
-    user: promise2 || null,
+    photo: promise1,
+    user: promise2,
   };
 }
