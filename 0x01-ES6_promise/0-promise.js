2664171a58869b2f9284358963db336227a78e49
Isaiah peter <peterisaiah4fun@gmail.com>
2023-01-26 01:03:55 +0100

    fix eslint

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,8 @@
 export default function getResponseFromAPI() {
-    const newPromise = new Promise((resolve, reject) => {
-        return resolve
-    });
+  const newPromise = new Promise((resolve, reject) => {
+    resolve();
+    reject();
+  });
 
-    return newPromise
+  return newPromise;
 }
--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,16 +1,14 @@
 export default function getResponseFromAPI(success) {
-    const newPromise = new Promise((resolve, reject) => {
-        if (success) {
-            resolve(
-                {
-                    status: 200,
-                    body: 'Success'
-                }
-            )
-        } else {
-            reject(new Error("The fake API is not working currently"))
-        }
-    });
+  const newPromise = new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
 
-    return newPromise
+  return newPromise;
 }
