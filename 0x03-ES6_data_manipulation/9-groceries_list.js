7c147bf314aaaeda1ef550f6f297fb869f45a4c5
Isaiah peter <peterisaiah4fun@gmail.com>
2023-01-31 21:55:53 +0100

    task 9 and 10

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -4,7 +4,7 @@ const groceriesList = () => {
   map.set('Apples', 10);
   map.set('Tomatoes', 10);
   map.set('Pasta', 1);
-  map.set('rice', 1);
+  map.set('Rice', 1);
   map.set('Banana', 5);
 
   return map;
