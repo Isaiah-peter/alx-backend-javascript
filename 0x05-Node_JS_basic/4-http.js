e6db0a5e31e0ce4cc9ed70c70cd19e4364171ada
Isaiah peter <peterisaiah4fun@gmail.com>
2023-03-21 20:57:40 +0100

    http module to create server

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const app = http.createServer();
+const PORT = 1245;
+
+app.on('request', (_, res) => {
+  const ouput = 'Hello Holberton School!';
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', ouput.length);
+  res.statue = 200;
+
+  res.write(Buffer.from(ouput));
+});
+
+app.listen(PORT);
