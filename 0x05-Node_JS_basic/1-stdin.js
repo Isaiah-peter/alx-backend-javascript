8fedd3cb5aae360f4310b378265354cfb818a742
Isaiah peter <peterisaiah4fun@gmail.com>
2023-03-21 08:29:16 +0100

    remove displaymessge

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,12 @@
-const displayMessage = require("./0-console");
-
-displayMessage("Welcome to Holberton School, what is your name?");
-
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
 process.stdin.on("readable", () => {
   const name = process.stdin.read();
 
   if (name) {
-    displayMessage(`Your name is: ${name}`);
+    process.stdout.write("Your name is: " + name);
   }
 });
 
 process.stdin.on("end", () => {
-  displayMessage("This important software is now closing");
+  process.stdout.write("This important software is now closing\n");
 });
