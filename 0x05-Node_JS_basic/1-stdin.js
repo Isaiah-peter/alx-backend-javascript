a14669e4ce9992e7c78f61f1598b0de93259ff33
Isaiah peter <peterisaiah4fun@gmail.com>
2023-03-21 07:41:55 +0100

    fix error

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,15 @@
-const displayMessage = require('./0-console')
+const displayMessage = require("./0-console");
 
-displayMessage("Welcome to Holberton School, what is your name?")
+displayMessage("Welcome to Holberton School, what is your name?");
 
-process.stdin.on('readable', () => {
-    const name = process.stdin.read()
+process.stdin.on("readable", () => {
+  const name = process.stdin.read();
 
-    if (name) {
-        displayMessage(`Your name is: ${name}`)
-    }
-})
+  if (name) {
+    displayMessage(`Your name is: ${name}`);
+  }
+});
 
-process.stdin.on('end', () => {
-    displayMessage("This important software is now closing")
-})
+process.stdin.on("end", () => {
+  displayMessage("This important software is now closing");
+});
