113939e234086adbc4f698eef86b0015585c9132
Isaiah peter <peterisaiah4fun@gmail.com>
2023-03-21 21:57:23 +0100

    task 7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,11 +1,10 @@
-const http = require('http');
+const express = require('express');
 const fs = require('fs');
 
-const app = http.createServer();
-const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
-
+const app = express();
 const PORT = 1245;
 const HOST = 'localhost';
+const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
 
 const countStudents = (dataPath) =>
   new Promise((resolve, reject) => {
@@ -63,53 +62,25 @@ const countStudents = (dataPath) =>
     }
   });
 
-const SERVER_ROUTE_HANDLERS = [
-  {
-    route: '/',
-    handler(_, res) {
-      const responseText = 'Hello Holberton School!';
+app.get('/', (_, res) => {
+  res.send('Hello Holberton School!');
+});
 
-      res.setHeader('Content-Type', 'text/plain');
-      res.setHeader('Content-Length', responseText.length);
-      res.statusCode = 200;
+app.get('/students', (_, res) => {
+  const responseParts = ['This is the list of our students'];
+  countStudents(DB_FILE)
+    .then((report) => {
+      responseParts.push(report);
+      const responseText = responseParts.join('\n');
+
       res.write(Buffer.from(responseText));
-    },
-  },
-  {
-    route: '/students',
-    handler(_, res) {
-      const responseParts = ['This is the list of our students'];
+    })
+    .catch((err) => {
+      responseParts.push(err instanceof Error ? err.message : err.toString());
+      const responseText = responseParts.join('\n');
 
-      countStudents(DB_FILE)
-        .then((report) => {
-          responseParts.push(report);
-          const responseText = responseParts.join('\n');
-          res.setHeader('Content-Type', 'text/plain');
-          res.setHeader('Content-Length', responseText.length);
-          res.statusCode = 200;
-          res.write(Buffer.from(responseText));
-        })
-        .catch((err) => {
-          responseParts.push(
-            err instanceof Error ? err.message : err.toString()
-          );
-          const responseText = responseParts.join('\n');
-          res.setHeader('Content-Type', 'text/plain');
-          res.setHeader('Content-Length', responseText.length);
-          res.statusCode = 200;
-          res.write(Buffer.from(responseText));
-        });
-    },
-  },
-];
-
-app.on('request', (req, res) => {
-  for (const routeHandler of SERVER_ROUTE_HANDLERS) {
-    if (routeHandler.route === req.url) {
-      routeHandler.handler(req, res);
-      break;
-    }
-  }
+      res.write(Buffer.from(responseText));
+    });
 });
 
 app.listen(PORT, HOST, () => {
